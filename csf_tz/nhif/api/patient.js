84f6665aa2844bc644d286d681e94388e3365fcf
Youssef Restom <youssefrestom@gmail.com>
2020-11-11 16:14:16 +0000

    feat: When the patient record is already saved with information 2 column dialog should come which shows previous values vs new values and asking user to confirm if the changes are to be done

--- csf_tz/nhif/api/patient.js
+++ csf_tz/nhif/api/patient.js
@@ -16,9 +16,6 @@ frappe.ui.form.on('Patient', {
         frm.trigger('get_patinet_info')
     },
     get_patinet_info: function (frm) {
-        // if (!frm.doc.insurance_subscription || !frm.doc.billing_item) {
-        //     return
-        // }
         frappe.call({
             method: 'csf_tz.nhif.api.patient.get_patinet_info',
             args: {
@@ -27,15 +24,85 @@ frappe.ui.form.on('Patient', {
             callback: function (data) {
                 if (data.message) {
                     const card = data.message
-                    frm.set_value("first_name", card.FirstName)
-                    frm.set_value("last_name", card.LastName)
-                    frm.set_value("patient_name", card.FullName)
-                    frm.set_value("sex", card.Gender)
-                    frm.set_value("dob", card.DateOfBirth)
-                    frm.set_value("product_code", card.ProductCode)
-                    frm.set_value("membership_no", card.MembershipNo)
+                    if (!frm.is_new()) {
+                        const d = new frappe.ui.Dialog({
+                            title: "Patient's information",
+                            primary_action_label: 'Submit',
+                            primary_action(values) {
+                                update_patient_info(frm, card)
+                                d.hide();
+                            }
+                        });
+                        $(`<div class="modal-body ui-front">
+                        <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Field Name</th>
+                                <th>Current Values</th>
+                                <th>New Values</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>First Name</td>
+                                <td>${frm.doc.first_name}</td>
+                                <td>${card.FirstName}</td>
+                            </tr>
+                            <tr>
+                                <td>Last Name</td>
+                                <td>${frm.doc.last_name}</td>
+                                <td>${card.LastName}</td>
+                            </tr>
+                            <tr>
+                                <td>Full Name</td>
+                                <td>${frm.doc.patient_name}</td>
+                                <td>${card.FullName}</td>
+                            </tr>
+                            <tr> 
+                                <td>Gender</td>
+                                <td>${frm.doc.sex}</td>
+                                <td>${card.Gender}</td>
+                            </tr>
+                             <tr>
+                                <td>Date of birth</td>
+                                <td>${frm.doc.dob}</td>
+                                <td>${card.DateOfBirth.slice(0, 10)}</td>
+                            </tr>
+                            <tr>
+                                <td>Product Code</td>
+                                <td>${frm.doc.product_code}</td>
+                                <td>${card.ProductCode}</td>
+                            </tr>
+                            <tr>
+                                <td>Membership No</td>
+                                <td>${frm.doc.membership_no}</td>
+                                <td>${card.MembershipNo}</td>
+                            </tr>
+                        </tbody>
+                        </table>
+                    </div>`).appendTo(d.body);
+                        d.show();
+                    }
+                    else {
+                        update_patient_info(frm, card)
+                    }
                 }
             }
         });
     },
 })
+
+function update_patient_info(frm, card){
+    frm.set_value("first_name", card.FirstName)
+    frm.set_value("last_name", card.LastName)
+    frm.set_value("patient_name", card.FullName)
+    frm.set_value("sex", card.Gender)
+    frm.set_value("dob", card.DateOfBirth)
+    frm.set_value("product_code", card.ProductCode)
+    frm.set_value("membership_no", card.MembershipNo)
+    frm.save()
+    frappe.show_alert({
+    message:__("Patient's information is updated"),
+    indicator:'green'
+    }, 5);
+}
