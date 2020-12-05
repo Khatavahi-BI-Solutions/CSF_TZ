ddcc9b66c912c3519e465d0b357cb9503946b933
Vimal <info@aakvatech.com>
2020-12-05 21:30:04 +0000

    fix: location of presribe field in hso and correct company selection in insurance claim

--- csf_tz/nhif/api/insurance_claim.py
+++ csf_tz/nhif/api/insurance_claim.py
@@ -20,6 +20,10 @@ def set_patient_encounter(doc, method):
 def set_price(doc, method):
     price_list = None
     price_list_rate = None
+    if doc.reference_dt == "Healthcare Service Order":
+        company = frappe.get_value(doc.reference_dt, doc.reference_dn, "company")
+    else:
+        company = frappe.defaults.get_user_default("Company")
     if  doc.insurance_subscription:
         hic_plan = frappe.get_value(
             "Healthcare Insurance Subscription", doc.insurance_subscription, "healthcare_insurance_coverage_plan")
@@ -27,7 +31,7 @@ def set_price(doc, method):
             "Healthcare Insurance Coverage Plan", hic_plan, "price_list")
         if price_list:
             if price_list_rate and price_list_rate != 0:
-                price_list_rate = get_item_price(doc.service_item, price_list, doc.company)
+                price_list_rate = get_item_price(doc.service_item, price_list, company)
                 doc.price_list_rate = price_list_rate
                 return
 
@@ -36,7 +40,7 @@ def set_price(doc, method):
         "Healthcare Insurance Company", doc.insurance_company, "default_price_list")
     if not price_list:
             frappe.throw(_("Please set Price List in Healthcare Insurance Coverage Plan"))
-    price_list_rate = get_item_price(doc.service_item, price_list, doc.insurance_company)
+    price_list_rate = get_item_price(doc.service_item, price_list, company)
     if price_list_rate == 0:
         frappe.throw(_("Please set Price List for item: {0}").format(doc.service_item))
     doc.price_list_rate = price_list_rate
