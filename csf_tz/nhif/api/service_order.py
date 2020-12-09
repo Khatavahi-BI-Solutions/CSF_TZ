e2ed1fd61d69703cfeee4e423265d67e3fc8fce3
Youssef Restom <youssefrestom@gmail.com>
2020-12-09 15:26:50 +0000

    feat: Unset the (order)_created = 0 in the child table record on Patient Encounter And set the Prescribe checkbox of child record to 1

--- csf_tz/nhif/api/service_order.py
+++ csf_tz/nhif/api/service_order.py
@@ -17,6 +17,7 @@ def set_missing_values(doc, method):
 
 @frappe.whitelist()
 def clear_insurance_details(service_order):
+    from csf_tz import console
     service_order_doc = frappe.get_doc('Healthcare Service Order', service_order)
     if service_order_doc.docstatus != 0:
         return
@@ -34,6 +35,25 @@ def clear_insurance_details(service_order):
     insurance_claim_doc.reload()
     insurance_claim_doc.delete()
     frappe.db.commit()
+
+    child_tables = {
+		# "drug_prescription": "",
+		"lab_test_prescription": "lab_test_created", #
+		"procedure_prescription": "procedure_created", #
+		"radiology_procedure_prescription": "radiology_examination_created",
+		# "therapies": "",
+	}
+    if service_order_doc.order_reference_doctype and service_order_doc.order_reference_name:
+        child_row_doc = frappe.get_doc(service_order_doc.order_reference_doctype, service_order_doc.order_reference_name)
+        parentfield = child_row_doc.get("parentfield")
+        created_field = child_tables.get(parentfield)
+        if not created_field:
+            return
+        setattr(child_row_doc, created_field, 0)
+        child_row_doc.prescribe = 1
+        child_row_doc.db_update()
+        frappe.db.commit()
+
     frappe.msgprint(_('Healthcare Insurance Claim {0} deleted successfully.').format(
                 frappe.bold(insurance_claim)), alert=True)
     return True
