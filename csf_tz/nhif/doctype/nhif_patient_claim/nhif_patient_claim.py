eb6d094da06ffa1ed6466f3765e8d8ace2fdeb15
Youssef Restom <youssefrestom@gmail.com>
2020-12-17 19:59:55 +0000

    update: NHIF Patient Claim DocType

--- csf_tz/nhif/doctype/nhif_patient_claim/nhif_patient_claim.py
+++ csf_tz/nhif/doctype/nhif_patient_claim/nhif_patient_claim.py
@@ -28,7 +28,7 @@ class NHIFPatientClaim(Document):
 		self.claim_year = int(now_datetime().strftime("%Y"))
 		self.claim_month = int(now_datetime().strftime("%m"))
 		self.folio_no = int(self.name[-9:])
-		self.created_by = frappe.session.full_name or "Administrator"
+		self.created_by = frappe.get_value("User", frappe.session.user, "full_name")
 		final_patient_encounter = self.get_final_patient_encounter()
 		self.practitioner_no = frappe.get_value("Healthcare Practitioner", final_patient_encounter.practitioner, "tz_mct_code")
 		if not self.practitioner_no:
@@ -39,6 +39,7 @@ class NHIFPatientClaim(Document):
 		appointment_type = frappe.get_value("Patient Appointment", self.patient_appointment, "appointment_type")
 		self.patient_type_code = "OUTPATIENT" if appointment_type != "In Patient" else "IN PATIENT"
 		self.patient_file_no = self.get_patient_file_no()
+		self.set_patient_claim_disease()
 
 
 	def get_patient_encounters(self):
@@ -49,6 +50,44 @@ class NHIFPatientClaim(Document):
 		 }
 		)
 		return patient_encounters
+
+
+	def set_patient_claim_disease(self):
+		patient_encounters = self.get_patient_encounters()
+		self.nhif_patient_claim_disease = []
+		diagnosis_list = []
+		for encounter in patient_encounters:
+			encounter_doc = frappe.get_doc("Patient Encounter", encounter.name)
+			for row in encounter_doc.patient_encounter_preliminary_diagnosis:
+				if row.code in diagnosis_list:
+					continue
+				diagnosis_list.append(row.code)
+				new_row = self.append("nhif_patient_claim_disease", {})
+				new_row.diagnosis_type = "Preliminary Diagnosis"
+				new_row.patient_encounter = encounter.name
+				new_row.codification_table = row.name
+				new_row.folio_disease_id = str(uuid.uuid1())
+				new_row.folio_id = self.folio_id
+				new_row.medical_code = row.medical_code
+				new_row.disease_code = row.code
+				new_row.description = row.description
+				new_row.created_by = frappe.get_value("User", row.modified_by, "full_name")
+				new_row.date_created = row.modified.strftime("%Y-%m-%d")
+			for row in encounter_doc.patient_encounter_final_diagnosis:
+				if row.code in diagnosis_list:
+					continue
+				diagnosis_list.append(row.code)
+				new_row = self.append("nhif_patient_claim_disease", {})
+				new_row.diagnosis_type = "Final Diagnosis"
+				new_row.patient_encounter = encounter.name
+				new_row.codification_table = row.name
+				new_row.folio_disease_id = str(uuid.uuid1())
+				new_row.folio_id = self.folio_id
+				new_row.medical_code = row.medical_code
+				new_row.disease_code = row.code
+				new_row.description = row.description
+				new_row.created_by = frappe.get_value("User", row.modified_by, "full_name")
+				new_row.date_created = row.modified.strftime("%Y-%m-%d")
 
 	
 	def get_final_patient_encounter(self):
